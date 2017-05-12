50e0e24d5e16def849ba29543dae3857f905745e
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2017-05-12 10:46:47 -0300

    Upando versão 1.2
    Adcionado buasca em largura

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class PathFinder
+{
+
+    public static List<Grid.Position> FindPath(Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
+    {
+        var path = new List<Grid.Position>();
+        Queue<Grid.Position> q = new Queue<Grid.Position>();
+        q.Enqueue(fromPosition);
+        
+        while (q.Count > 0)
+        {
+            var p = q.Dequeue();
+            path.Add(p);
+            if (p.x == toPosition.x)
+            {
+                Debug.Log("Achei");
+                break;
+            }
+
+            else
+            {
+                if (p.y >= 0 && p.y < 9) { q.Enqueue(new Grid.Position { x = p.x, y = p.y + 1 }); }
+                if (p.y > 0 && p.y =< 9) { q.Enqueue(new Grid.Position { x = p.x, y = p.y - 1 }); }
+                if (p.x >= 0 && p.x < 9) { q.Enqueue(new Grid.Position { x = p.x + 1, y = p.y }); }
+                if (p.x > 0 && p.x =< 9) { q.Enqueue(new Grid.Position { x = p.x - 1, y = p.y }); }
+            }
+         }
+
+        return path;
+    }
+}
---
+++ fd/AStar-master/Assets/Scripts/PathFinder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PathFinder
+{
+
+    public static List<Grid.Position> FindPath(Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
+    {
+        int x = fromPosition.x;
+        int y = fromPosition.y;
+        int i;
+        var path = new List<Grid.Position>();
+        var middlePosition = new Grid.Position();  
+
+        path.Add(fromPosition);
+        path.Add(middlePosition);
+        path.Add(toPosition);
+
+        for ( i = 0; i <= x; i++ )
+        {
+            middlePosition.x = fromPosition.x + i;
+           // Debug.Log("Valor do i " + i);
+            if(i == x) { i = 0; }
+        }
+
+        for (i = 0; i <= y; i++)
+        {
+            middlePosition.y = fromPosition.y + i;
+            if (i == y) { i = 0; }
+        }
+
+        Debug.Log("Posição inicial: " + fromPosition);
+        Debug.Log("Posição final: " + toPosition);
+        Debug.Log("Posição do meio: " + middlePosition);
+
+        return path;
+
+    }
+}
