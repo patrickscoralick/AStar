e95e7a0b77ca7f17ca7cb82171b769f2f2907bdc
Patrick Scoralick Costa <patrickscoralick4@gmail.com>
2017-05-21 18:02:06 -0300

    Upando Versão Final
    2º Bimestre - AV1 - Programação de Jogos

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -10,26 +10,36 @@ public static class PathFinder
         var path = new List<Grid.Position>();
         Queue<Grid.Position> q = new Queue<Grid.Position>();
         q.Enqueue(fromPosition);
-        
-        while (q.Count > 0)
+        Grid.Position[,] caminho = new Grid.Position[tiles.GetLength(0),tiles.GetLength(1)];
+
+        var p = q.Dequeue();
+
+        while (p.x != toPosition.x || p.y != toPosition.y)
         {
-            var p = q.Dequeue();
-            path.Add(p);
-            if (p.x == toPosition.x)
+            if (p.x == toPosition.x && p.y == toPosition.y)
             {
                 Debug.Log("Achei");
                 break;
             }
-
             else
             {
-                if (p.y >= 0 && p.y < 9) { q.Enqueue(new Grid.Position { x = p.x, y = p.y + 1 }); }
-                if (p.y > 0 && p.y =< 9) { q.Enqueue(new Grid.Position { x = p.x, y = p.y - 1 }); }
-                if (p.x >= 0 && p.x < 9) { q.Enqueue(new Grid.Position { x = p.x + 1, y = p.y }); }
-                if (p.x > 0 && p.x =< 9) { q.Enqueue(new Grid.Position { x = p.x - 1, y = p.y }); }
+                if (p.x != toPosition.x)
+                {
+                    if (p.x < toPosition.x)
+                        p.x++;
+                    else
+                        p.x--;
+                }
+                if (p.y != toPosition.y)
+                {
+                    if (p.y < toPosition.y)
+                        p.y++;
+                    else
+                        p.y--;
+                }
+                path.Add(p);
             }
-         }
-
+        }
         return path;
     }
 }
